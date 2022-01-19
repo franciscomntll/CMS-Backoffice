292f7d0f398e5008fea11881ddd89c56732571e8
Francisco <ciscomontilla@gmail.com>
2022-01-19 16:49:57 -0400

    modified slug.js

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,56 +1,89 @@
 import { Box, Button, Flex } from "@chakra-ui/react";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useReducer, useState } from "react";
 import Datatable from "../components/Datatable";
 import { FetchGraphQL } from "../Fetching";
 
+class Action {
+  type;
+  data;
+  constructor(type, data) {
+    this.type = type;
+    this.data = data;
+  }
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "VIEW":
+      return new Action("view", action.payload);
+    case "EDIT":
+      return new Action("edit", action.payload);
+    case "CREATE":
+      return new Action("create", action.payload);
+    case "DELETE":
+      return new Action("delete", action.payload);
+    default:
+      break;
+  }
+};
+
 const Module = ({ slug }) => {
-  const initialValues = {total: null, results: []}
+  const initialValues = { total: null, results: [] };
   const [data, setData] = useState(initialValues);
-  
-  
+  const [action, setAction] = useReducer(reducer, new Action("view", {}));
 
   const fetchBusiness = async () => {
-   try {
-    const result = await FetchGraphQL.getBusinessAll()
-    setData(result)
-   } catch (error) {
-     console.log(error);
-     return null
-   }
-     
-  }
+    try {
+      const result = await FetchGraphQL.getBusinessAll();
+      setData(result);
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  };
 
   const ObjectData = {
-    business : fetchBusiness
-  }
+    business: fetchBusiness,
+    categoriesBusiness: () => console.log("categorias"),
+  };
 
   useEffect(() => {
-    console.log(slug);
-    setData(initialValues)
-    ObjectData[slug] && ObjectData[slug]()
+    setData(initialValues);
+    setAction({ type: "VIEW", payload: {} });
+    ObjectData[slug] && ObjectData[slug]();
   }, [slug]);
 
   const columns = useMemo(
     () => [
       {
-        Header: "Title",
+        Header: "Nombre de la empresa",
         accessor: "businessName",
       },
       {
         Header: "ID",
         accessor: "_id",
       },
-      
     ],
     []
   );
+
   return (
     <Flex as={"section"} flexDir={"column"} gap={"1rem"}>
-      <Button w={"fit-content"} px={"1rem"}>
+      {action.type === "view" && <Button
+        w={"fit-content"}
+        px={"1rem"}
+        onClick={() => setAction({ type: "CREATE", payload: {} })}
+      >
         Añadir registro
-      </Button>
+      </Button>}
       <Box bg={"white"} p={"1rem"} shadow={"sm"} rounded={"xl"}>
-        <Datatable columns={columns} data={data.results} />
+        {action.type === "view" && <Datatable columns={columns} data={data.results} />}
+        {action.type === "create" && (
+          <>
+          <Button onClick={() => setAction({type: "VIEW", payload:{}})}>Atras</Button>
+          <p>Crear Registro Formulario</p>
+          </>
+        )}
       </Box>
     </Flex>
   );
@@ -60,6 +93,6 @@ export default Module;
 
 export async function getServerSideProps({ params }) {
   return {
-    props: params, // will be passed to the page component as props
+    props: params, // { slug : "business"}
   };
 }
